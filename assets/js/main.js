dcaa322c751701784de0d87f6032eb91751488da
[KamilMammadli] <kamilfm@code.edu.az>
2021-06-20 23:49:50 +0400

    final project

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,14 @@
 AOS.init();
+
+$(document).ready(function ($) {
+
+    let tabs = $(".tabs li a");
+
+    tabs.click(function () {
+        let content = this.hash.replace('/', '');
+        tabs.removeClass("active");
+        $(this).addClass("active");
+        $(".footer-second").find('div').hide();
+        $(content).fadeIn(200);
+    })
+});
